4e3ea4e72fe0bd800bb1a244f3d468ae4c0b4164
Michel Siqueira Reis <michelcyc@gmail.com>
2024-09-24 14:03:05 -0300

    Atualizando
    Atualizando

--- Michel_ProgramacaoParalela/Trab1_prog_paralela/main.c
+++ Michel_ProgramacaoParalela/Trab1_prog_paralela/main.c
@@ -1,28 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+#include "help.h"
+#include "kmeans.h"
 
-int main()
-{
-    int n_clusters;
-    printf("Insira o numero de clusters\n");
-    if (scanf("%d", &n_clusters) == 1) {
-        printf("You entered the integer: %d\n", n_clusters);
+int main(int argc, char **argv) {
+	if (argc < 6) {
+		puts("Not enough parameters...");
+		exit(1);
+	}
+	const int n = atoi(argv[2]), m = atoi(argv[3]), k = atoi(argv[4]);
+	if ((n < 1) || (m < 1) || (k < 1) || (k > n)) {
+		puts("Values of input parameters are incorrect...");
+		exit(1);
+	}
+	double *x = (double*)malloc(n * m * sizeof(double));
+	if (x == NULL) {
+		puts("Memory allocation error...");
+		exit(1);
+	}
+	int *y = (int*)malloc(n * sizeof(int));
+	if (y == NULL) {
+		puts("Memory allocation error...");
+		free(x);
+		exit(1);
+	}
+	fscanf_data(argv[1], x, n * m);
 
-        double x1[] = {1.0, 2.0};
-        double x2[] = {4.0, 6.0};
-        int dimensions = 2;
+	clock_t cl = clock();
+	kmeans(x, y, n, m, k);
+	cl = clock() - cl;
+	if (argc > 6) {
+		int *ideal = (int*)malloc(n * sizeof(int));
+		if (ideal == NULL) {
+			fprintf_result(argv[5], y, n);
+		} else {
+			fscanf_splitting(argv[6], ideal, n);
+			const double a = get_accuracy(ideal, y, n);
+			printf("Accuracy of k-means clustering = %.5lf;\n", a);
+			fprintf_full_result(argv[5], y, n, a);
+			free(ideal);
+		}
+	} else {
+		fprintf_result(argv[5], y, n);
+	}
+	printf("Time for k-means clustering = %.6lf s.;\nThe work of the program is completed...\n", (double)cl / CLOCKS_PER_SEC);
 
-        // Call the get_distance function
-        double distance_squared = get_distance(x1, x2, dimensions);
-
-        // Print the result
-        printf("The squared Euclidean distance between x1 and x2 is: %.2f\n", distance_squared);
-
-
-
-    } else {
-        printf("Input invalido.\n");
-    }
-    return 0;
+	free(x);
+	free(y);
+	return 0;
 }
